34dc55b36eddf4fe00b5d7a572418a6baa6f3974
Lawrence <lawrence.ssonko@hotmail.com>
2019-02-24 23:01:04 +0000

    All mechanics
    All game mechanics work

--- Chronos/Assets/Scripts/Enemy/EnemyShoot.cs
+++ Chronos/Assets/Scripts/Enemy/EnemyShoot.cs
@@ -4,45 +4,41 @@ using UnityEngine;
 
 public class EnemyShoot : MonoBehaviour
 {
-        public GameObject bulletPrefab;
-        public Transform bulletSpawn;
+        public GameObject bulletPrefab;//to take the bullets prefab and instantiate
+        public Transform bulletSpawn;//the set spawnPoint 
         public float bulletSpeed = 30;//how fast the bullet will fly
         public float lifeTime = 3;//how long the bullet will last
-        public float waitTime;
-        private float currentTime;
-        private bool shot;
-        private Transform bulletSpawned;
-    
-        public GameObject player;
-    
-    
-    
+        public float waitTime;//The time to wait until firing again
+        private float currentTime;//Checking the time  until firing 
+        private bool shot;//verifies if anything has been shot
+        private Transform bulletSpawned;//Checks if the bullet has been spwaned
+        public GameObject player;//Takes the position of the player
+
         public void Start()
         {
             player = GameObject.FindWithTag("Player");
         }
-    
-    
+
         // Update is called once per frame
         void Update()
         {
-            this.transform.LookAt(player.transform);
+            this.transform.LookAt(player.transform);//tells the object to look at the player
     
-            if (currentTime == 0)
+            if (currentTime == 0)//if the currentTime counter is 0
             {
-                Fire();
+                Fire();//run the fire method
             }
     
     
-            if (shot && currentTime < waitTime)
+            if (shot && currentTime < waitTime)//if the bullet hasn't been shot and the current time is 0
             {
-                currentTime += 1 * Time.deltaTime;
+                currentTime += 1 * Time.deltaTime;//increments the counting by 1 in the worlds set timescale
             }
     
     
             if (currentTime >= waitTime)
             {
-                currentTime = 0;
+                currentTime = 0;// makes sure that the current time will set to 0
             }
     
         }
@@ -55,7 +51,6 @@ public class EnemyShoot : MonoBehaviour
             /*bulletSpawned = Instantiate(bulletPrefab.transform, bulletSpawn.transform.position, Quaternion.identity);
             bulletSpawned.transform.rotation = this.transform.rotation;*/
             GameObject bullet = Instantiate(bulletPrefab);//puts the bullet in the scene
-            bullet.name = "bullet";
             bullet.transform.position = bulletSpawn.position;//spawns bullet in the set spawnpoint
             Vector3 rotation = bullet.transform.rotation.eulerAngles;//converts rotation angles to vector 3
             bullet.transform.rotation = Quaternion.Euler(rotation.x, transform.eulerAngles.y, rotation.z); //keeps same rotation of bullet, makes it face forward
---
+++ Chronos/Assets/Scripts/Player/PlayerDelete.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;//to change scene
+
+public class PlayerDelete : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)//triggers if there is a collision
+    {
+        if(other.tag == "EnemyBullet")//if the enemy bullet collides with my player
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);//resets the scene
+
+        }
+    }
+}
