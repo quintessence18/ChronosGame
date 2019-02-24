345f8c4aa01979c10bafa1b70d95a8da6fb30e66
Lawrence <lawrence.ssonko@hotmail.com>
2019-02-24 20:30:53 +0000

    PlayerMovement
    Created my tutorial world and the player can move

---
+++ Chronos/Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(PlayerMotor))]//requires my player motor script
+public class PlayerController : MonoBehaviour
+{
+
+    [SerializeField]//my variable will show up in the inspector even though it is private
+    private float speed = 5f;
+    [SerializeField]
+    private float lookSensitivity = 3f;//allows me to determine the look sensitivity 
+
+
+    private PlayerMotor motor;
+
+    void Start()
+    {
+        //get motor from beginning
+        motor = GetComponent<PlayerMotor>();//we don't have to check for playermotor since w know its there
+    }
+
+    void Update()
+    {
+        //calculate movement velocity as 3D Vector
+        float xMov = Input.GetAxisRaw("Horizontal"); //store horizontal movement as float, raw val to allow movemnt smoothing control if wanted
+        float zMov = Input.GetAxisRaw("Vertical"); //store Vertical movement as float, raw val to allow movemnt smoothing control if wanted
+
+        Vector3 movHorizontal = transform.right * xMov;//takes into consideration on where we are facing opposed to vector3.right, relative to player not world
+        Vector3 movVertical = transform.forward * zMov;
+
+        //movment vector final
+        Vector3 _velocity = (movHorizontal + movVertical).normalized * speed; //total length of combine vector should be one, thus a direction vector multiplied by speed
+
+
+        //apply movement
+        motor.Move(_velocity);
+
+
+
+        //calculate rotation as 3D Vector to be able to turn around.
+        float yRot = Input.GetAxisRaw("Mouse X"); //takes the mouse input of it's position
+
+        Vector3 _rotation = new Vector3(0f, yRot, 0f) * lookSensitivity;//calculates
+
+        //apply Rotation
+        motor.Rotate(_rotation);
+
+        //calculate  camera rotation as 3D Vector to be able to look up.
+        float xRot = Input.GetAxisRaw("Mouse Y"); //takes the mouse input of it's position
+        Vector3 _cameraRotation = new Vector3(xRot, 0f, 0f) * lookSensitivity;//calculates
+
+        //apply cameraRotation
+        motor.RotateCamera(_cameraRotation);
+    }
+
+}
---
+++ Chronos/Assets/Scripts/Player/PlayerMotor.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]//we want to have a rigid body using our player motor
+public class PlayerMotor : MonoBehaviour
+{
+    //player controller uses velocity to contorl player
+    private Vector3 velocity = Vector3.zero; //defaults the velocity to 0 if not moving
+    private Vector3 rotation = Vector3.zero; //defaults rotation to 0 if not moving
+    private Vector3 cameraRotation = Vector3.zero;//defaults rotation to 0 if not moving
+
+    [SerializeField]
+    private Camera cam;//allows me to assign the pllayer's camera in the inspeco
+
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();//we know we have a rigidbody
+    }
+
+    //gets movement vector
+    public void Move(Vector3 _velocity)
+    {
+        velocity = _velocity;//assigns inputted velocity and assigns it to velocity variable
+    }
+
+
+    //takes the rotation vector for the camera's horizontal
+    public void Rotate(Vector3 _rotation)
+    {
+        rotation = _rotation;//assigns passed camera rotation and assigns it to rotation variable
+    }
+
+    //takes the rotation vector for the camera's vertical
+    public void RotateCamera(Vector3 _cameraRotation)
+    {
+        cameraRotation = _cameraRotation;//assigns passed camera rotation and assigns it to rotation variable
+    }
+
+
+    //computes calculations per frame, used for rigidbodies
+    void Update()
+    {
+        //performs movement
+        PerformMovement();
+        PerformRotation();
+    }
+
+    //perform movement based on velicocity var
+    void PerformMovement()
+    {
+        if (velocity != Vector3.zero) //checks if the ridgibody is still
+        {
+            rb.MovePosition(rb.position + velocity * Time.fixedDeltaTime);//moves position on the fixed rate by the vector var
+        }
+    }
+
+    //perfrom rotation Method
+    void PerformRotation()
+    {
+        rb.MoveRotation(rb.rotation * Quaternion.Euler(rotation));//calculates the roation angles for us
+        cam.transform.Rotate(-cameraRotation);
+    }
+}
