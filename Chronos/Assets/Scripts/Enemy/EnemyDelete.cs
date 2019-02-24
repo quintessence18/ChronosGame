9fb77fc4543bfd886c6ab894ddaa585735177899
Lawrence <lawrence.ssonko@hotmail.com>
2019-02-24 22:08:51 +0000

    Files
    Everything is working apart from the robot deleting

--- Chronos/Assets/Scripts/Enemy/EnemyDelete.cs
+++ Chronos/Assets/Scripts/Enemy/EnemyDelete.cs
@@ -4,15 +4,15 @@ using UnityEngine;
 
 public class EnemyDelete : MonoBehaviour
 {
+    [SerializeField] PlayerData playerdata;
     public int score;//created a new variable score to pass the value to player data
-
 
     private void OnTriggerEnter(Collider other)// acts when it collides with a game object
     {
-        if (other.tag == "Bullet")//checks for the object with a tag called Bullet(my bullet object in game)
+        if (other.tag == "bullet")//checks for the object with a tag called Bullet(my bullet object in game)
         {
-            PlayerManager.currentData.Score += 10;//sends and stores score addition in my player manager
-            Destroy(this.gameObject);//removes object from game when both colliders meet/touch
+            playerdata.Score += 10;//sends and stores score addition in my player manager
+            Destroy(gameObject);//removes object from game when both colliders meet/touch
             Debug.Log("Destroyed");
         }
     }
